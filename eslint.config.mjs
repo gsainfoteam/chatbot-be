4821f5b43ab023785042745cbf78893c329e42b8
ikjunchoi <choiikjun1101@gmail.com>
2026-01-11 01:15:26 +0900

    chore: update ESLint configuration to enforce stricter TypeScript rules and adjust Prettier settings for specific files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -37,10 +37,18 @@ export default tseslint.config(
       'require-await': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-assignment': 'warn',
+      '@typescript-eslint/no-unsafe-member-access': 'warn',
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
+  },
+  {
+    files: ['libs/infoteam-idp/**'],
+    rules: {
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
-      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );

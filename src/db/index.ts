5a45a59317fc03e26f66b11a8eeb7f16f719e2fe
ikjunchoi <choiikjun1101@gmail.com>
2026-01-24 23:27:22 +0900

    refactor: remove unused type import from database index file

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,6 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
-import type { Options } from 'postgres';
 import * as schema from './schema';
 
 // Database connection token

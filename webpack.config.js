b671607b09c15fc6c7ae282f388e09df5ccf052e
2paperstar <lee@paperst.ar>
2026-09-25 06:29:41 +0900

    Revert "fix: update webpack config to match bbosong-be for proper path tracing"
    This reverts commit f39426c4dffb4f91e150e5fe4109bbb3b3589bc0.

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,22 @@
-// webpack.config.js
-const nodeExternals = require('webpack-node-externals');
-const fs = require('node:fs');
-const glob = require('glob');
-const path = require('node:path');
+const webpack = require('webpack');
+const path = require('path');
 
-const modules = glob.sync('./node_modules/*/package.json', {
-  ignore: ['node_modules/.*/**'],
-});
-const packages = modules
-  .map((module) => {
-    try {
-      return JSON.parse(fs.readFileSync(module, 'utf8'));
-    } catch {
-      return null;
-    }
-  })
-  .filter(Boolean);
-
-const allowlist = packages
-  .filter((packageJson) => packageJson.type === 'module')
-  .map((packageJson) => packageJson.name)
-  .filter(Boolean)
-  .map((name) => new RegExp(`^${name.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}`));
-
-module.exports = function (options) {
+/**
+ * @param {webpack.Configuration} options
+ * @param {typeof webpack} webpackInstance
+ * @returns {webpack.Configuration}
+ */
+module.exports = function (options, webpackInstance) {
   return {
     ...options,
-    entry: options.entry,
-    externals: [nodeExternals({ allowlist })],
-    resolve: {
-      ...options.resolve,
-      extensionAlias: {
-        '.js': ['.ts', '.js'],
-      },
-    },
     entry: {
       main: path.resolve(__dirname, 'src/main.ts'),
       instrumentation: path.resolve(__dirname, 'src/instrumentation.mts'),
     },
     output: {
-      ...options.output,
-      path: path.resolve(__dirname, 'dist'),
       filename: '[name].js',
-      libraryTarget: 'commonjs2',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
     },
   };
 };
